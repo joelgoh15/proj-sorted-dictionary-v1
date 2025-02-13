a6fc674c9ea93621f513f94902b833f2ffe789bf
Joel Goh Chong Hui <147377225+joelgoh15@users.noreply.github.com>
2025-02-13 17:27:05 +0800

    upload project files.

---
+++ ClassSortedDictionary.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace project11_2
+{
+    internal class ClassSortedDictionary
+    {
+        public ClassSortedDictionary() { }
+
+        public static SortedDictionary<string, int> CollectWords(string input)
+        {
+
+            SortedDictionary<string, int> dictionary =
+               new SortedDictionary<string, int>();
+
+
+            //auto checks for any '&' or ' ' in a string in order to remove it
+            if (input.Contains(" ") || input.Contains("&"))
+            {
+                
+                string[] words = Regex.Split(input, @"[&\s]+");
+
+                foreach (var word in words)
+                {
+                    string wordKey = word.ToLower();
+
+                    if (dictionary.ContainsKey(wordKey))
+                    {
+                        ++dictionary[wordKey];
+                    }
+                    else
+                    {
+                        dictionary.Add(wordKey, 1);
+                    }
+                }
+            }
+            return dictionary;
+        }
+
+
+        public static void DisplayDictionary<K, V>(
+           SortedDictionary<K, V> dictionary)
+        {
+            Console.WriteLine("\nSorted dictionary contains:\n{0,-12}{1,-12}",
+               "Key:", "Value:");
+
+            foreach (K key in dictionary.Keys)
+                Console.WriteLine("{0,-12}{1,-12}", key, dictionary[key]);
+
+            Console.WriteLine("\nsize: {0}", dictionary.Count);
+        }
+
+    }//end-class
+}//end-namespace
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project11_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("an applica");
+            
+            SortedDictionary<string, int> dictionary;
+            string input; 
+            input = "This  is & an& example & string";
+            
+            dictionary = ClassSortedDictionary.CollectWords(input);
+            ClassSortedDictionary.DisplayDictionary(dictionary);
+
+            //exit application
+            Console.ReadLine();
+
+        }//end-main
+
+    }//end-class
+}//end-namespace
